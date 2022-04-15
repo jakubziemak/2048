54f14c70f4e857694a2a9a1372538a0426f88afa
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2022-04-15 15:47:26 +0200

    game class, gameboard generation

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
+class Game {
+    constructor(){
+        this.newTile()
+        this.newTile()
+        console.table(this.gameBoard)
+    }
+    
+    gameBoard = [[0, 0, 0, 0], 
+                 [0, 0, 0, 0], 
+                 [0, 0, 0, 0], 
+                 [0, 0, 0, 0]]
 
+    newTile = () => {
+        const val = Math.random() < 0.5 ? 2 : 4
+        const randomPos = Math.floor(Math.random() * this.freeSpaces().length)
+        const [posX, posY] = this.freeSpaces()[randomPos]
 
+        this.gameBoard[posX][posY] = val        
+    }
+    freeSpaces = () => {
+        const free = this.gameBoard
+            .flat()
+            .map((cell, i) => {if (cell == 0) return [Math.floor(i/4), i%4]})
+            .filter(cell => cell !==undefined)
+        return free
+    }
+}
 
 
 let Colors = {

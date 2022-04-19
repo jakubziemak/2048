463818b6ee624d78cca314822aa0f0beae7129dc
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2022-04-19 21:23:47 +0200

    ability to move tiles in gameboard array

--- main.js
+++ main.js
@@ -24,6 +24,66 @@ class Game {
             .filter(cell => cell !==undefined)
         return free
     }
+    move = (direction) => {
+        switch(direction){
+            case 'ArrowLeft':
+                this.moveLeft()
+                break
+            case 'ArrowUp':
+                this.moveUp()
+                break
+            case 'ArrowRight':
+                this.moveRight()
+                break
+            case 'ArrowDown':
+                this.moveDown()
+                break
+        }
+        console.table(this.gameBoard)
+    }
+    moveUp = () => {
+        this.cellsToMove().forEach(cell => {
+            const free = this.freeSpaces().filter(([x, y]) => y == cell.posY)[0]
+            if(free && cell.posX > free[0]){
+                this.gameBoard[cell.posX][cell.posY] = 0
+                this.gameBoard[free[0]][free[1]] = cell.val
+            }
+        })
+    }
+    moveDown = () => {
+        this.cellsToMove().reverse().forEach(cell => {
+            const free = this.freeSpaces().filter(([x, y]) => y == cell.posY).reverse()[0]
+            if(free && cell.posX < free[0]){
+                this.gameBoard[cell.posX][cell.posY] = 0
+                this.gameBoard[free[0]][free[1]] = cell.val
+            }
+        })
+    }
+    moveLeft = () => {
+        this.cellsToMove().forEach(cell => {
+            const free = this.freeSpaces().filter(([x, y]) => x == cell.posX)[0]
+            if(free && cell.posY > free[1]){
+                this.gameBoard[cell.posX][cell.posY] = 0
+                this.gameBoard[free[0]][free[1]] = cell.val
+            }
+        })
+    }
+    moveRight = () => {
+        this.cellsToMove().reverse().forEach(cell => {
+            const free = this.freeSpaces().filter(([x, y]) => x == cell.posX).reverse()[0]
+            if(free && cell.posY < free[1]){
+                this.gameBoard[cell.posX][cell.posY] = 0
+                this.gameBoard[free[0]][free[1]] = cell.val
+            }
+        })
+    }
+    cellsToMove = () => {
+        const cells = this.gameBoard
+            .flat()
+            .map((val, i) => {if (val !== 0) return {val: val, posX: Math.floor(i/4), posY: i%4}})
+            .filter(cell => cell !== undefined)
+        return cells
+    }
 }
 
 

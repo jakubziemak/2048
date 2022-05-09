00108d0064949e5c2a2405a5b01505e82c51217f
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2022-05-09 12:02:24 +0200

    code updated with prettier extension

--- modules/tile.js
+++ modules/tile.js
@@ -1,90 +1,92 @@
-export default class Tile{
-    constructor(value, x, y, id){
-        this.value = value
-        this.x = x
-        this.y = y
-        this.id = id
-        
-        this.width = document.querySelector('td').clientWidth
-        this.height = document.querySelector('td').clientHeight
-        this.spacing = Number(getComputedStyle(document.querySelector('table')).borderSpacing[0])
-        
-        this.createTile()
-    }
-    colors = {
-        2: '#eee6db',
-        4: '#ece0c8',
-        8: '#efb27c',
-        16: '#f3966a',
-        32: '#f27d62',
-        64: '#f46042',
-        128: '#ebce77',
-        256: '#eccb67',
-        512: '#edc759',
-        1024: '#e7c257',
-        2048: '#e8bf4e'
-    }
-    
-    createTile = () => {
-        const div = document.querySelector('#tiles-handler')
-        const newTile = document.createElement('div')
-        
-        newTile.id = `tile${this.id}`
-        newTile.className = 'tile'
-        newTile.innerText = this.value
-        
-        newTile.style.height = this.height + 'px'
-        newTile.style.width = this.width + 'px'
-        newTile.style.background = this.colors[this.value]
-        
-        div.appendChild(newTile)
-        this.tile = document.querySelector(`#tile${this.id}`)
+export default class Tile {
+  constructor(value, x, y, id) {
+    this.value = value;
+    this.x = x;
+    this.y = y;
+    this.id = id;
 
-        this.moveTo()
-        this.newTileAnimation()
-    }
-    
-    newPosition = (newX, newY) => {
-        const oldX = this.x
-        const oldY = this.y
+    this.width = document.querySelector("td").clientWidth;
+    this.height = document.querySelector("td").clientHeight;
+    this.spacing = Number(
+      getComputedStyle(document.querySelector("table")).borderSpacing[0]
+    );
 
-        this.x = newX
-        this.y = newY
+    this.createTile();
+  }
+  colors = {
+    2: "#eee6db",
+    4: "#ece0c8",
+    8: "#efb27c",
+    16: "#f3966a",
+    32: "#f27d62",
+    64: "#f46042",
+    128: "#ebce77",
+    256: "#eccb67",
+    512: "#edc759",
+    1024: "#e7c257",
+    2048: "#e8bf4e",
+  };
 
-        this.moveTo()
-        this.movingAnimation(oldX, oldY, this.x, this.y)
-    }
+  createTile = () => {
+    const div = document.querySelector("#tiles-handler");
+    const newTile = document.createElement("div");
 
-    moveTo = () => {
-        this.tile.style.top = this.y * (this.height + this.spacing) + 'px'
-        this.tile.style.left = this.x * (this.height + this.spacing) + 'px'
-    }
+    newTile.id = `tile${this.id}`;
+    newTile.className = "tile";
+    newTile.innerText = this.value;
 
-    movingAnimation = (oldX, oldY, newX, newY) => {
-        const ani = [
-            {
-                top: oldY * (this.height + this.spacing) + 'px',
-                left: oldX * (this.height + this.spacing) + 'px'
-            },
-            {
-                top: newY * (this.height + this.spacing) + 'px',
-                left: newX * (this.height + this.spacing) + 'px'
-            }
-        ]
+    newTile.style.height = this.height + "px";
+    newTile.style.width = this.width + "px";
+    newTile.style.background = this.colors[this.value];
 
-        this.tile.animate(ani, 100)
-    }
-    
-    newTileAnimation = () => {
-        const ani = [
-            {transform: 'scale(1)'},
-            {transform: 'scale(1.1)'},
-            {transform: 'scale(1)'}
-        ]
-        this.tile.animate(ani, 100)
-    }
+    div.appendChild(newTile);
+    this.tile = document.querySelector(`#tile${this.id}`);
 
-    delete = () => {
-        this.tile.remove()
-    }
+    this.moveTo();
+    this.newTileAnimation();
+  };
+
+  newPosition = (newX, newY) => {
+    const oldX = this.x;
+    const oldY = this.y;
+
+    this.x = newX;
+    this.y = newY;
+
+    this.moveTo();
+    this.movingAnimation(oldX, oldY, this.x, this.y);
+  };
+
+  moveTo = () => {
+    this.tile.style.top = this.y * (this.height + this.spacing) + "px";
+    this.tile.style.left = this.x * (this.height + this.spacing) + "px";
+  };
+
+  movingAnimation = (oldX, oldY, newX, newY) => {
+    const ani = [
+      {
+        top: oldY * (this.height + this.spacing) + "px",
+        left: oldX * (this.height + this.spacing) + "px",
+      },
+      {
+        top: newY * (this.height + this.spacing) + "px",
+        left: newX * (this.height + this.spacing) + "px",
+      },
+    ];
+
+    this.tile.animate(ani, 100);
+  };
+
+  newTileAnimation = () => {
+    const ani = [
+      { transform: "scale(1)" },
+      { transform: "scale(1.1)" },
+      { transform: "scale(1)" },
+    ];
+    this.tile.animate(ani, 100);
+  };
+
+  delete = () => {
+    this.tile.remove();
+  };
 }
